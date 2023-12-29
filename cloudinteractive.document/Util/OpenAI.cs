5fc5bbee6e5878438dc7300ec950b587acff360d
Coppermine-SP <son5121502@gmail.com>
2023-12-29 23:58:37 +0900

    feat: OpenAI 모듈

--- cloudinteractive.document/Util/OpenAI.cs
+++ cloudinteractive.document/Util/OpenAI.cs
@@ -1,13 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.Json.Nodes;
+using OpenAI_API;
+using OpenAI_API.Chat;
+using OpenAI_API.Models;
 
 namespace cloudinteractive.document.Util
 {
     public static class OpenAI
     {
+        private static string? _key;
+        public static void Init(string key)
+        {
+            _key = key;
+        }
 
+        public static async Task<string> GetChatCompletionAsync(string prompt, string[] document, Model model)
+        {
+            const int maxLength = 2000;
+            var builder = new StringBuilder();
+            foreach (string page in document)
+            {
+                if (builder.Length < maxLength)
+                {
+                    builder.Append(page);
+                }
+            }
+
+            ChatRequest request = new ChatRequest()
+            {
+                Model = model,
+                Temperature = 0.0,
+                ResponseFormat = ChatRequest.ResponseFormats.JsonObject,
+                Messages = new List<ChatMessage>()
+                {
+                    new ChatMessage(ChatMessageRole.System, prompt),
+                    new ChatMessage(ChatMessageRole.User, builder.ToString())
+                }
+            };
+            OpenAIAPI api = new OpenAIAPI(_key);
+
+            var response = await api.Chat.CreateChatCompletionAsync(request);
+
+            return response.ToString();
+        }
     }
 }
